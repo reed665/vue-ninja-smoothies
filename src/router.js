4c73cba2a1101b6d56df59e379ab77c2d9e7d1b5
Alexander Buiko <reed665@hotmail.com>
2018-05-03 16:42:57 +0300

    setup AddSmoothie view component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from './views/Index.vue'
+
+import Index from '@/views/Index.vue'
+import AddSmoothie from '@/views/AddSmoothie.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index,
+    },
+    {
+      path: '/add-smoothie',
+      name: 'addSmoothie',
+      component: AddSmoothie,
     },
   ]
 })
---
+++ src/views/AddSmoothie.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+
+    <form>
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an Ingredient</label>
+        <input type="text" name="add-ingredient">
+      </div>
+
+      <div class="field center-align">
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.add-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.field {
+  margin: 20px auto;
+}
+</style>
