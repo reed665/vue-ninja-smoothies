7949d75326ca282683388be378666241b53bfd38
Alexander Buiko <reed665@hotmail.com>
2018-05-03 09:01:09 +0300

    add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Ninja Smoothies</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+  nav {
+    padding: 0 20px;
+  }
+</style>
