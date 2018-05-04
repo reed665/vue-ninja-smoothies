b3433304c77aa7fde24698926e4bcfac508de519
Alexander Buiko <reed665@hotmail.com>
2018-05-04 09:32:10 +0300

    connect to EditSmoothie view

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import Index from '@/views/Index.vue'
 import AddSmoothie from '@/views/AddSmoothie.vue'
+import EditSmoothie from '@/views/EditSmoothie.vue'
 
 Vue.use(Router)
 
@@ -18,5 +19,10 @@ export default new Router({
       name: 'addSmoothie',
       component: AddSmoothie,
     },
+    {
+      path: '/edit-smoothie/:slug',
+      name: 'editSmoothie',
+      component: EditSmoothie,
+    }
   ]
 })
---
+++ src/views/EditSmoothie.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit a Smoothie: {{ slug }}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    slug () {
+      return this.$route.params.slug
+    }
+  }
+}
+</script>
